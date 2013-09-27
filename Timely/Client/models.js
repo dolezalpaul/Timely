c26834a3cbf81edda2b7e737c51d439beeb8ecbe
Premysl Krajcovic <premyslkrajcovic@gmail.com>
2013-09-27 09:13:10 +0200

    Add Ember Data Models

--- Timely/Client/models.js
+++ Timely/Client/models.js
@@ -10,3 +10,17 @@ App.User = DS.Model.extend({
     name: attr({ type: 'string' }),
     email: attr({ type: 'string' })
 });
+
+App.Project = DS.Model.extend({
+    name: attr({ type: 'string' })
+});
+
+App.Task = DS.Model.extend({
+    name: attr({ type: 'string' })
+});
+
+App.Favorite = DS.Model.extend({
+    user: DS.belongsTo('user'),
+    project: DS.belongsTo('project'),
+    task: DS.belongsTo('task')
+});
